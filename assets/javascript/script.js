bc21def2aac0c36e9a3cafa750e0dcc01df5bedc
Sean Oh <seanoh@ucsb.edu>
2022-06-23 11:53:05 -0700

    write pseudocode for game logic

---
+++ assets/javascript/script.js
@@ -0,0 +1,20 @@
+// Declare variables for player and computer scores and ties
+// Create an array with options rock, paper, scissors
+// Use random number generator for the computer's choice of r, p, s
+// Use conditionals to determine who wins
+//    if player's choice === computer's choice, we have a tie
+//    if player selects rock and computer selects paper, then computer wins
+//    else if player selects rock and computer selects scissors, then player wins
+//    else if player selects paper and computer selects scissors, then player wins
+//    etc...
+// After each round, ask if the player wants to do another round
+// Keep track of player wins, losses and ties
+
+
+var startButton = document.querySelector("#start-game");
+startButton.addEventListener("click",playGame);
+
+function playGame() {
+
+
+}
