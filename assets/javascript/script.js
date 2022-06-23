6321d8bfa2178bf51f9dc3ccc464e050e2568afb
Sean Oh <seanoh@ucsb.edu>
2022-06-23 12:57:11 -0700

    add game functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,14 +12,65 @@
 
 
 var startButton = document.querySelector("#start-game");
-startButton.addEventListener("click",playGame);
+startButton.addEventListener("click", playGame);
 
-const options = ["R","P","S"];
+const options = ["R", "P", "S"];
 var numWins = 0;
 var numLosses = 0;
 var numTies = 0;
 
 function playGame() {
-  var userChoice = prompt("Enter R, P, or S to select your choice.");
+	startButton.innerHTML = "Continue";
 
+  var userChoice = promptUser();
+  if(!userChoice) return;
+
+  var computerChoice = options[Math.floor(Math.random() * 3)];
+  console.log(computerChoice);
+  alert(`Computer chose ${computerChoice}`);
+  displayWinner(userChoice, computerChoice);
+  displayScore();
+  if(confirm("Play Again?")){
+  	playGame();
+  }
+
+}
+
+function promptUser() {
+  var userChoice = prompt("Enter R, P, or S to select your choice.");
+  userChoice = userChoice.toUpperCase();
+  if (options.indexOf(userChoice) === -1) {
+    var tryAgain = confirm("Invalid input! Try again?");
+    if (tryAgain) {
+      promptUser();
+    } else{
+    	return;
+    }
+  }
+  return userChoice;
+}
+
+function displayWinner(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    alert("You tie!");
+    numTies++;
+    return;
+  } else if ((userChoice === "R" && computerChoice === "S") ||
+    (userChoice === "P" && computerChoice === "R") ||
+    (userChoice === "S" && computerChoice === "P")
+  ) {
+    alert("You win!");
+    numWins++;
+    return;
+  } else {
+    alert("You lose :(");
+    numLosses++;
+    return;
+  }
+}
+
+function displayScore() {
+	alert("Current Score:\nWins: " + numWins +
+  			"\nLosses: " + numLosses + 
+        "\nTies: " + numTies);
 }
